0ba2da5dcf47368c2439fe687e6b8538685454c2
DhruvAtreja <51291353+DhruvAtreja@users.noreply.github.com>
2022-12-13 11:11:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,27 +11,35 @@ function checkword(word) {
 
 document.querySelector("button").addEventListener('click', () => {
     output.innerHTML = "";
-    const words = input.value.split(" ");
-    console.log("a");
-    let i = 1;
+    let summarisedInput;
+    let data = new FormData();
+    let obj;
+    data.append('text', input.value);
+    fetch('https://sea-lion-app-jnxtu.ondigitalocean.app/predict', {
+        method: 'POST',
+        body: data
+    })
+        .then(response => response.json())
+        .then((response) => {
+            console.log(response);
+            obj = response;
 
-    for (let word of words) {
-        if (checkword(word) && (word.length > 2)) {
-            let n = word.length;
-            word = word.replace(word.substring(0, (n + 1) / 2), '<b>' + word.substring(0, (n + 1) / 2) + '</b>');
-            output.innerHTML += word + " ";
-        }
-        else {
-            output.innerHTML += '<b>' + word + '</b>' + " ";
-            console.log(word);
-        }
-        i++;
-    }
+            const words = obj[0].summary_text.split(" ");
+            console.log("a");
+            let i = 1;
+            for (let word of words) {
+                if (checkword(word) && (word.length > 2)) {
+                    let n = word.length;
+                    word = word.replace(word.substring(0, (n + 1) / 2), '<b>' + word.substring(0, (n + 1) / 2) + '</b>');
+
+                    output.innerHTML += word + " ";
+                }
+                else {
+                    output.innerHTML += '<b>' + word + '</b>' + " ";
+                    console.log(word);
+                }
+                i++;
+            }
+        });
+
 });
-
-
-
-
-
-
-
